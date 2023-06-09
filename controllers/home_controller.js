11d7571381fb43566ee490fbfbedff40833ce404
Harsh <harshsantoshi05@gmail.com>
2023-06-09 13:29:33 +0530

    Rendered view home from home controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,6 @@
 module.exports.home = function(req, res){
-    // res.send("<h1>This is from send</h1>");
-    return res.send("<h1>Express is running</h1>");
+    // return res.send("<h1>Express is running</h1>");
+    return res.render('home',{
+        title : "Home"
+    })
 }
